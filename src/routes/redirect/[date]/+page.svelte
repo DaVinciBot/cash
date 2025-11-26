8e342e78b435655095767cd0ccc2bb7971fe3be5
UrbsKali <urbain.lantres@tutanota.com>
2025-11-26 03:48:50 +0100

    add redirect for hdd

---
+++ src/routes/redirect/[date]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const date = $page.params?.date || '2025-11-26_03-40-22';
+		window.location.href = `https://jzxajbetkioeihoihpkn.supabase.co/storage/v1/object/public/hdd/${date}`;
+	});
+</script>
+
+<p>Redirecting...</p>
