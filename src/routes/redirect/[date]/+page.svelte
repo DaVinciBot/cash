51ad5828386e685214c8d16659e6702cef73cdb9
UrbsKali <urbain.lantres@tutanota.com>
2025-11-26 03:59:28 +0100

    oscour hdd

--- src/routes/redirect/[date]/+page.svelte
+++ src/routes/redirect/[date]/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
 
-	onMount(() => {
-		const date = $page.params?.date || '2025-11-26_03-40-22';
-		window.location.href = `https://jzxajbetkioeihoihpkn.supabase.co/storage/v1/object/public/hdd/${date}`;
-	});
+	$: date = $page.params['date'] || '2025-11-26_03-40-22';
+	$: imageUrl = `https://jzxajbetkioeihoihpkn.supabase.co/storage/v1/object/public/hdd/${date}`;
 </script>
 
-<p>Redirecting...</p>
+<div class="fixed inset-0 z-50 flex items-center justify-center w-screen h-screen bg-black">
+	<img src={imageUrl} alt="Full screen" class="object-contain w-full h-full" />
+</div>
