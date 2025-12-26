f3370f61d0daf6d15f0aaa55dbc9fcc45bb994e0
UrbsKali <urbain.lantres@tutanota.com>
2025-12-26 20:03:22 +0100

    remove custom lib to add submodule

--- src/lib/components/markdown/Paragraph.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let pre = false;
-</script>
-
-{#if pre}
-	<pre class="mb-2 font-mono text-sm text-white"><slot /></pre>
-{:else}
-	<p class="mb-2 text-base text-justify text-white"><slot /></p>
-{/if}
--- src/lib/components/markdown/Table.svelte
+++
@@ -1,5 +0,0 @@
-<div class="overflow-x-auto">
-	<table class="w-full border-collapse">
-		<slot />
-	</table>
-</div>
