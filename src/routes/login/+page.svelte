ac5381eb3d9b8342bea288c9ba99fdc32a707851
austin <eliottroussille@gmail.com>
2026-04-11 11:42:31 +0200

    feat: implement login page and enhance redirection logic

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { base } from '$app/paths';
+	import AuthForm from '$lib/components/utils/AuthForm.svelte';
+</script>
+
+<section class="min-h-screen min-w-screen">
+	<AuthForm redirect_uri={`${base}/`} />
+</section>
